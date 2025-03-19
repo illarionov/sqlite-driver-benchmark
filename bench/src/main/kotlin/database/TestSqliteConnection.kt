04187fbca4f0fea027386826b6330d0cd2df86df
Alexey Illarionov <alexey@0xdc.ru>
2025-03-19 19:29:46 +0300

    Various updates 2

--- bench/src/main/kotlin/database/TestSqliteConnection.kt
+++ bench/src/main/kotlin/database/TestSqliteConnection.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2025, the wasm-sqlite-open-helper project authors and contributors. Please see the AUTHORS file
+ * for details. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package at.released.sqlitedriverbenchmark.database
 
 import androidx.sqlite.SQLiteConnection
@@ -41,4 +47,3 @@ class TestSqliteConnection private constructor(
         }
     }
 }
-
